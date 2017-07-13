6d030459b56f8354d9c8bdd7a768bdf60aa0cbaf
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 15:12:15 +0000

    integreated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,5 +14,9 @@ module ApplicationHelper
       greeting = "Thanks for visiting me from #{session[:source]} and you on are the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
     end
-  end 
+  end
+  
+  def copyright_generator
+    BurtonViewTool::Renderer.copyright 'Tanner Burton', 'All rights reserved'
+  end
 end
